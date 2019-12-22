3b5f6d1464fa8b133d676d6c438cd04ef80abb54
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-12-21 17:03:25 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
-# your code goes here
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :happiness,:hygiene,:name
+
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+end
