251c8859e08ddb552d03b3b34039457555f29f58
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-12-21 17:04:46 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -11,4 +11,14 @@ class Person
     @hygiene = 8
   end
 
+  def happiness=(idx)
+    if idx < 0
+      @happiness = 0
+    elsif idx > 10
+      @happiness = 10
+    else
+      @happiness = idx
+    end
+  end
+
 end
