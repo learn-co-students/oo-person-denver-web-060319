3128bb3786edebe3eb7b16effcdf219358e4479c
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-12-23 18:48:31 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -31,6 +31,9 @@ class Person
     end
   end
 
+  def clean?
+    @hygiene > 7
+  end
 
   def happy?
     if self.happiness > 7
@@ -49,5 +52,31 @@ class Person
   def take_bath
     self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      person.happiness -= 2
+      self.happiness-= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      person.happiness += 1
+      self.happiness += 1
+      "blah blah sun blah rain"
+  else
+    "blah blah blah blah blah"
+  end
   end
 end
